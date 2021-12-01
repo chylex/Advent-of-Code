270e3e2349f73596b3d3c022907f1c5b3d86a4f4
chylex <contact@chylex.com>
2021-12-01 06:38:18 +0100

    Add 2021 - Day 1 - Part 2

--- 2021/01/main.kt
+++ 2021/01/main.kt
@@ -2,6 +2,10 @@ import java.io.File
 
 fun main() {
 	val lines = File("input/1.txt").readLines().map(String::toInt)
+	
 	val totalIncreases = lines.windowed(2).count { it[1] > it[0] }
+	val windowedIncreases = lines.windowed(3).map(List<Int>::sum).windowed(2).count { it[1] > it[0] }
+	
 	println("Total Increases: $totalIncreases")
+	println("Windowed Increases: $windowedIncreases")
 }
