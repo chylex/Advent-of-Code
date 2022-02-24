68fa8a157c63e236c54bdf7111099d4de5a75917
chylex <contact@chylex.com>
2022-02-24 04:33:11 +0100

    Add 2020 - Day 5 - Part 1

---
+++ 2020/05/main.rs
@@ -0,0 +1,72 @@
+use std::error::Error;
+
+use crate::pass::BoardingPass;
+
+#[path = "../utils/mod.rs"]
+mod utils;
+
+fn main() -> Result<(), Box<dyn Error>> {
+	let passes = utils::parse_input_lines::<BoardingPass>()?;
+	
+	let max_seat_id = passes.iter().map(BoardingPass::get_seat_id).max().unwrap_or(0);
+	println!("Max seat ID: {}", max_seat_id);
+	
+	Ok(())
+}
+
+mod pass {
+	use std::str::FromStr;
+	
+	use crate::utils::GenericError;
+	
+	pub struct BoardingPass {
+		row_directions: [bool; 7],
+		col_directions: [bool; 3],
+	}
+	
+	impl BoardingPass {
+		pub fn find_seat(&self) -> (u32, u32) {
+			fn combine_bits(items: &[bool]) -> u32 {
+				items.iter().enumerate().filter(|(_, half)| **half).map(|(i, _)| 1u32 << (items.len() - i - 1)).reduce(|a, b| a | b).unwrap_or(0)
+			}
+			
+			return (combine_bits(&self.row_directions), combine_bits(&self.col_directions));
+		}
+		
+		pub fn get_seat_id(&self) -> u32 {
+			let (row, col) = self.find_seat();
+			return (row * (self.row_directions.len() as u32 + 1)) + col;
+		}
+	}
+	
+	impl FromStr for BoardingPass {
+		type Err = GenericError;
+		
+		fn from_str(s: &str) -> Result<Self, Self::Err> {
+			fn get_mut_at<'a>(arr: &'a mut [bool], index: usize, error_prefix: &'static str) -> Result<&'a mut bool, GenericError> {
+				arr.get_mut(index).ok_or_else(|| GenericError::new(format!("{} out of bounds: {}", error_prefix, index)))
+			}
+			
+			let mut row_directions = [false; 7];
+			let mut col_directions = [false; 3];
+			
+			for (i, c) in s.chars().take(row_directions.len()).enumerate() {
+				*get_mut_at(&mut row_directions, i, "Row direction")? = match c {
+					'B' => Ok(true),
+					'F' => Ok(false),
+					_ => Err(GenericError::new(format!("Unknown row direction: {}", c)))
+				}?;
+			}
+			
+			for (i, c) in s.chars().skip(row_directions.len()).enumerate() {
+				*get_mut_at(&mut col_directions, i, "Column direction")? = match c {
+					'R' => Ok(true),
+					'L' => Ok(false),
+					_ => Err(GenericError::new(format!("Unknown col direction: {}", c)))
+				}?;
+			}
+			
+			Ok(BoardingPass { row_directions, col_directions })
+		}
+	}
+}
