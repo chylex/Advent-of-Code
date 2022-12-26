20f7f156e038921afc53a70287210a0956954426
chylex <contact@chylex.com>
2022-12-26 07:59:39 +0100

    Add 2022 - Day 6 - Part 2

--- 2022/06/main.py
+++ 2022/06/main.py
@@ -4,9 +4,17 @@ from utils.input import read_input_lines
 
 input_sequence = read_input_lines()[0]
 
-buffer = collections.deque(maxlen = 4)
-for i, c in enumerate(input_sequence):
-    buffer.append(c)
-    if len(set(buffer)) == 4:
-        print(f"Found start-of-packet marker at: {i + 1}")
-        break
+
+def find_position_with_distinct_characters(count: int) -> int:
+    buffer = collections.deque(maxlen = count)
+    
+    for i, c in enumerate(input_sequence):
+        buffer.append(c)
+        if len(set(buffer)) == count:
+            return i + 1
+        
+    return -1
+
+
+print(f"Found start-of-packet marker at: {find_position_with_distinct_characters(4)}")
+print(f"Found start-of-message marker at: {find_position_with_distinct_characters(14)}")
